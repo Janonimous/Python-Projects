6fbbb3e47192ac4dc0d1a853969e67be13d0b54f
Jaynako <79431682+Janonimous@users.noreply.github.com>
2021-08-09 17:53:09 -0700

    Update 2048.py

--- 2048.py/2048.py
+++ 2048.py/2048.py
@@ -5,7 +5,7 @@ from pygame.locals import *
 
 clock = pygame.time.Clock()
 pygame.init()
-pygame.display.set_caption('Get Angle Test')
+pygame.display.set_caption('2048')
 screen = pygame.display.set_mode((1000, 1000), 0, 32)
 
 '''
@@ -16,39 +16,56 @@ screen = pygame.display.set_mode((1000, 1000), 0, 32)
 
 class tile:
 
-    def __init__(self, x, y, width, height, type, multiplier=1):
-        self.x = x * multiplier + 125
-        self.y = y * multiplier + 125
+    tile_colors = {2: (238, 228, 218), 4: (237, 224, 200), 8: (242, 177, 121),
+    16: (245, 249, 99), 32: (246, 124, 95), 64: (246, 94, 59), 128: (237, 207, 114),
+    256: (237, 204, 97), 512: (237, 200, 80), 1024: (237, 197, 63), 2048: (237, 194, 96)}
+
+    def __init__(self, x, y, width, height, type):
+        self.x = x
+        self.y = y
         self.width = width
         self.height = height
         self.type = type
-        self.multiplier = multiplier # for displaying purposes
+        self.offset = [0, 0]
         self.rect = pygame.Rect(self.x, self.y, width, height)
+        self.color = self.get_color()
 
+    def get_color(self):
+        color = tile.tile_colors[self.type]
+        return color
+
+    def display(self, surf):
+        pygame.draw.rect(surf, self.color, self.rect)
+
+rows = 4 # length of columns
+columns = 4 # length of rows
+board_layout = [[y+(x*4)+1 for y in range(columns)] for x in range(rows)]
 board = {}
-for i in range(16):
-    x = (i % 4) +1
-    y = (i - (i % 4)) // 4 +1
-    board[i+1] = (x, y)
-
-'''board = {1: (1,1), 2: (2,1), 3: (3,1), 4: (4,1),
-        5: (1,2), 6: (2,2), 7: (3,2), 8: (4,2),
-        9: (1,3), 10: (2,3), 11: (3,3), 12: (4,3),
-        13: (1,4), 14: (2,4), 15: (3,4), 16: (4,4)}'''
-
-# make board dict have open bool & available_spaces derive values from board dict
-available_spaces = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+for i in range(rows*columns):
+    x = ((i % rows) * 125) + 250
+    y = (((i - (i % columns)) // 4) * 125) + 250
+    board[i+1] = {}
+    board[i+1]["coords"], board[i+1]["open"] = (x, y), True
 
 tiles = []
+available_spaces = []
 start = True
+
+def check_spaces():
+    #available_spaces = [key for key in board if board[key]["open"] == True]
+    for key in board:
+        if board[key]["open"] == True:
+            available_spaces.append(key)
 
 def generate_tile():
     if len(available_spaces) != 0:
         board_space = random.choice(available_spaces)
-        x, y = board[board_space]
-        t_obj = tile(x, y, 125, 125, 2, 125)
+        x, y = board[board_space]["coords"]
+        t_obj = tile(x, y, 125, 125, 2)
         tiles.append(t_obj)
         available_spaces.pop(available_spaces.index(board_space))
+
+check_spaces()
 
 
 while True:
@@ -64,7 +81,7 @@ while True:
     pygame.draw.rect(screen, (187, 173, 160), board_rect)
 
     for t in tiles:
-        pygame.draw.rect(screen, (238, 228, 218), t.rect)
+        t.display(screen)
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -75,12 +92,14 @@ while True:
                 pygame.quit()
                 sys.exit()
 
-            if event.key == K_SPACE:
+            if event.key == K_1:
                 start = True
                 tiles.clear()
                 available_spaces.clear()
                 for i in range(16):
                     available_spaces.append(i+1)
+            if event.key == K_2:
+                start = True
 
 
     pygame.display.update()
